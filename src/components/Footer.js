cd847c45fdc597fb5f4c69e9142a6804a230308b
chanduchaitanya <chanduchaiatnya406@gmail.com>
2026-03-31 01:38:31 +0530

    fourth

---
+++ src/components/Footer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo2.png";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Brand Section */}
+        <div className="footer-brand">
+          <Link to="/">
+            <img src={logo} alt="Zepfter Logo" className="footer-logo" />
+          </Link>
+          <p>
+            Bridging healthcare and technology for a smarter tomorrow. We provide cutting-edge solutions for modern challenges.
+          </p>
+          <div className="social-icons">
+            <a href="#" className="social-icon">IN</a>
+            <a href="#" className="social-icon">FB</a>
+            <a href="#" className="social-icon">TW</a>
+            <a href="#" className="social-icon">IG</a>
+          </div>
+        </div>
+
+        {/* Links Section */}
+        <div className="footer-links">
+          <h4>Quick Links</h4>
+          <ul>
+            <li><Link to="/">Why Zepfter</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/solutions">Solutions</Link></li>
+            <li><Link to="/courses">Courses</Link></li>
+          </ul>
+        </div>
+
+        {/* Contact Section */}
+        <div className="footer-contact">
+          <h4>Contact Us</h4>
+          <ul>
+            <li>Email: info@zepfter.com</li>
+            <li>Phone: +1 234 567 8900</li>
+            <li>Location: 123 Health Tech Ave, Innovation City, CA 90210</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Zepfter. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
+  return (
+    <nav className="navbar">
 
-            {/* Logo */}
-            <div className="logo">
-                <Link to="/">
-                    {/* We assume the provided logo image will be saved as logo.png in the public folder */}
-                    <img src="/logo.png" alt="Zepfter Logo" className="logo-img" />
-                </Link>
-            </div>
+      {/* Logo */}
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Zepfter Logo" className="logo-img" />
+        </Link>
+      </div>
 
-            {/* Menu */}
-            <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/solutions">Solutions</Link></li>
-                <li><Link to="/courses">Courses</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+      {/* Menu */}
+      <ul className="nav-links">
+        <li><Link to="/">Why Zepfter</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/solutions">Solutions</Link></li>
+        <li><Link to="/courses">Courses</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
 
-            {/* CTA Button */}
-            <div className="nav-btn">
-                <Link to="/auth">
-                    <button>Get Started</button>
-                </Link>
-            </div>
+      {/* CTA Button */}
+      <div className="nav-btn">
+        <Link to="/auth">
+          <button>Get Started</button>
+        </Link>
+      </div>
 
-        </nav>
-    );
+    </nav>
+  );
 }
 
 export default Navbar;
